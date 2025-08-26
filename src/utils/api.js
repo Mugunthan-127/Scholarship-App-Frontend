f14bae0c9c6ade93543b3291f11f81e3d1d771eb
Mugunthan M <mugunthanmuthuraman9@gmail.com>
2025-08-26 16:03:23 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 //api.js
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080/api';
+const API_BASE_URL = 'https://scholarship-app-backend-production.up.railway.app/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
